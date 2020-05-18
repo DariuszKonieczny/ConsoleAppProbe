0ccffffac0c3a82fd64014e44894f1ca0878f193
dariu <dariu@LEGION-DK-LAPTO>
2020-05-18 22:14:31 +0200

    Dodanie nowel linii

--- ConsoleAppProbe/Program.cs
+++ ConsoleAppProbe/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleAppProbe
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Next Line");
+
         }
     }
 }
